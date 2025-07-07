91f949e5c576183d3f7492950bff48e9a53fdeaa
PabloA009 <ayapan.pab009@gmail.com>
2025-07-07 16:11:38 -0600

    data type function

--- mon-ui/vite.config.js
+++ mon-ui/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: 'https://PabloA009.github.io//Utils-Api-Test-Postman',
+  base: 'https://PabloA009.github.io//Utils-Api-Test-Postman',
 });
